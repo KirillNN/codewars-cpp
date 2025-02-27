46eb04c98bb2026ffe77076400bdc03c8e0dda85
Kirill <wmforum@bk.ru>
2025-02-27 17:26:23 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
-#include "find_the_integral.h"  
-#include "exclamation_marks_series_11.h"  
+//#include "find_the_integral.h"  
+//#include "exclamation_marks_series_11.h"  
+#include "multi_table_for_num.h"  
 using namespace std;
 
 
@@ -10,15 +11,19 @@ int main()
     //cout << "Hello, World!" << endl; 
     
     //string x = integrate(3, 2);
-    string result = cw_replace("Hi!");
-    cout << result << endl;
-
-
+    //string result = cw_replace("Hi!");
     //int a = 0;
     //int b = 0;
     //cin >> a >> b;
     //double d = (double)a / b;
-    
+
     //cout << d;
+
+    string result = multi_table(5);
+
+
+    cout << result << endl;
+
+
     return 0;
 }
---
+++ multi_table_for_num.cpp
@@ -0,0 +1,9 @@
+#include "multi_table_for_num.h"
+
+#include <string>
+using namespace std;
+
+string multi_table(int number)
+{
+    return ""; // good luck
+}
